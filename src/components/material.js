ed255a58d83245921f4fe435a95d86acb352e708
Osariemen Aibueku <102379269+Osariemen7@users.noreply.github.com>
2024-04-15 14:58:09 +0100

    Add files via upload

---
+++ src/components/material.js
@@ -0,0 +1,143 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Typography, Button, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+export const BootstrapButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 39%',
+    border: '1px solid',
+    lineHeight: 2,
+    backgroundColor: '#0063cc',
+    borderColor: '#0063cc',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  });
+
+  export const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: '#0096FF',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#0096FF',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#E0E3E7',
+      },
+      '&:hover fieldset': {
+        borderColor: '#B2BAC2',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#6F7E8C',
+      },
+    },
+    width: '88%',
+    maxWidth: '100%' 
+    
+  });
+   
+  export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+  
+ export const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  }));
+
+
+  export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  export const ValidationTextField = styled(TextField)({
+    '& input:valid + fieldset': {
+      borderColor: '#E0E3E7',
+      borderWidth: 1,
+      padding: '5px'
+    },
+    '& input:invalid + fieldset': {
+      borderColor: 'red',
+      borderWidth: 1,
+    },
+    '& input:valid:focus + fieldset': {
+      borderLeftWidth: 4,
+      padding: '6px !important', // override inline-style
+    },
+    width: '88%',
+    maxWidth: '100%'
+    
+  });
+  
+  export const ValidationEyeField = styled(TextField)({
+    '& input:valid + fieldset': {
+      borderColor: '#E0E3E7',
+      borderWidth: 1,
+      padding: '5px'
+    },
+    '& input:invalid + fieldset': {
+      borderColor: 'red',
+      borderWidth: 1,
+    },
+    '& input:valid:focus + fieldset': {
+      borderLeftWidth: 4,
+      padding: '6px !important', // override inline-style
+    },
+    width: '88%',
+    maxWidth: '100%',
+    marginLeft: '5%'
+    
+  });
+  
